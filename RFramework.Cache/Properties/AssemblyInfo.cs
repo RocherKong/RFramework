55e0fb73f4c7cac6f473422e6a5632736d7cdb03
rocher <rocher@126.com>
2016-08-21 15:19:16 +0800

    Reduced version of Newtonsoft form 9.0.1 to 8.0.3 to adapt nuget version.

--- RFramework.Cache/Properties/AssemblyInfo.cs
+++ RFramework.Cache/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("RFramework.Cache")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("RFramework.Cache is Componet of Cache")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("i4ta")]
 [assembly: AssemblyProduct("RFramework.Cache")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
